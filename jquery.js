7c3a130550e284ecbe01086840cd367c2997c8ba
Sumit <sumit.gulati@berylsystems.com>
2020-11-11 18:15:12 +0530

    reverse an array

--- jquery.js
+++ jquery.js
@@ -1,102 +1,126 @@
 
-// Min and max valuen array
+// // Min and max valuen array
 
-array = [2, 5, 1, 50, 300, 999, 22]
-max = array[0];
-min = array[0];
-for(var i = 0; i < array.length; i++){
-    if(array[i] > max){
-      max= array[i];
-    }
-    if(array[i] < min){
-        min= array[i];
-    }
-}
-console.log(min, max)
+// array = [2, 5, 1, 50, 300, 999, 22]
+// max = array[0];
+// min = array[0];
+// for(var i = 0; i < array.length; i++){
+//     if(array[i] > max){
+//       max= array[i];
+//     }
+//     if(array[i] < min){
+//         min= array[i];
+//     }
+// }
+// console.log(min, max)
 
-// Find biggest number
+// // Find biggest number
 
-var a = 5;
-var b = 8;
-var c = 3;
-var d = 10;
-var e = 6;
+// var a = 5;
+// var b = 8;
+// var c = 3;
+// var d = 10;
+// var e = 6;
 
-if(a > b && a > c && a > d && a > e){
-  console.log("a is bigger");
-}
-if(b > a && b > c && b > d && b > e){
-  console.log("b is bigger");
-}
-if(c > a && c > b && c > d && c > e){
-  console.log("c is bigger");
-}
-if(d > a && d > b && d > c && d > e){
-  console.log("d is bigger");
-}
-if(e > a && e > b && e > c && e > d){
-  console.log("e is bigger");
-}
-
-
-// Even and Odd Nos
-
-var a = 9;
-if(a%2==0){
-  console.log("Even")
-}
-else{
-  console.log("Odd")
-}
-
-// Total sum of even numbers
-
-var a = 50;
-var b = 0;
-for(var i=1; i<= a; i++){
-
-    if(i%2 == 0){
-      b = i + b;
-    }
-    // if(i%2 !== 0){
-    //   b = i + b;
-    // }
-}
-console.log("Total sum of even numbers " +b);
-
-// Total sum of odd numbers
-
-var a = 50;
-var b = 0;
-for(var i=1; i<= a; i++){
-
-    if(i%2 !== 0){
-      b = i + b;
-    }
-}
-console.log("Total sum of odd numbers " +b);
-
-// Sum of Array elements
-var array = [10, 2, 40, 23, 40];
-var a = 0;
-for (var i = 0; i <array.length; i++){
-  a = a + array [i]
-
-  console.log(a)
-}
+// if(a > b && a > c && a > d && a > e){
+//   console.log("a is bigger");
+// }
+// if(b > a && b > c && b > d && b > e){
+//   console.log("b is bigger");
+// }
+// if(c > a && c > b && c > d && c > e){
+//   console.log("c is bigger");
+// }
+// if(d > a && d > b && d > c && d > e){
+//   console.log("d is bigger");
+// }
+// if(e > a && e > b && e > c && e > d){
+//   console.log("e is bigger");
+// }
 
 
-var arr = [1,2,3,4,5]
-var rev = arr.reverse();
-console.log(rev)
+// // Even and Odd Nos
 
-var num = [10,14,3,45,74,1,9]
+// var a = 9;
+// if(a%2==0){
+//   console.log("Even")
+// }
+// else{
+//   console.log("Odd")
+// }
 
-function sorting(a, b){
-  return (a-b);
+// // Total sum of even numbers
+
+// var a = 50;
+// var b = 0;
+// for(var i=1; i<= a; i++){
+
+//     if(i%2 == 0){
+//       b = i + b;
+//     }
+//     // if(i%2 !== 0){
+//     //   b = i + b;
+//     // }
+// }
+// console.log("Total sum of even numbers " +b);
+
+// // Total sum of odd numbers
+
+// var a = 50;
+// var b = 0;
+// for(var i=1; i<= a; i++){
+
+//     if(i%2 !== 0){
+//       b = i + b;
+//     }
+// }
+// console.log("Total sum of odd numbers " +b);
+
+// // Sum of Array elements
+// var array = [10, 2, 40, 23, 40];
+// var a = 0;
+// for (var i = 0; i <array.length; i++){
+//   a = a + array [i]
+
+//   console.log(a)
+// }
+
+
+// var arr = [1,2,3,4,5]
+// var rev = arr.reverse();
+// console.log(rev)
+
+// var num = [10,14,3,45,74,1,9]
+
+// function sorting(a, b){
+//   return (a-b);
   
+// }
+// console.log(num)
+
+
+var even = 0;
+var odd = 0;
+
+for(i = 1; i <= 50; i ++){
+  if(i%2 == 0)
+  even = even + i
+  else{
+    odd = odd + i
+  }
 }
-console.log(num)
+console.log(even, odd)
+
+
+// Reverse an array
+
+array = [2, 8, 5, 1, 9];
+for(i=4; i>=0; i--){
+  
+  console.log(array[i])
+
+}
+
 
 
 
