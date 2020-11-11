8c57f410454e20988bf4e62fac621f1780c2505c
Sumit <sumit.gulati@berylsystems.com>
2020-11-11 15:49:49 +0530

    find biggest number

--- jquery.js
+++ jquery.js
@@ -1,4 +1,6 @@
 
+// Min and max valuen array
+
 array = [2, 5, 1, 50, 300, 999, 22]
 max = array[0];
 min = array[0];
@@ -12,6 +14,33 @@ for(var i = 0; i < array.length; i++){
 }
 console.log(min, max)
 
+// Find biggest number
+
+var a = 5;
+var b = 8;
+var c = 3;
+var d = 10;
+var e = 6;
+
+if(a > b && a > c && a > d && a > e){
+  console.log("a is bigger");
+}
+if(b > a && b > c && b > d && b > e){
+  console.log("b is bigger");
+}
+if(c > a && c > b && c > d && c > e){
+  console.log("c is bigger");
+}
+if(d > a && d > b && d > c && d > e){
+  console.log("d is bigger");
+}
+if(e > a && e > b && e > c && e > d){
+  console.log("e is bigger");
+}
+
+
+// Even and Odd Nos
+
 var a = 9;
 if(a%2==0){
   console.log("Even")
@@ -19,6 +48,8 @@ if(a%2==0){
 else{
   console.log("Odd")
 }
+
+// Total sum of even numbers
 
 var a = 50;
 var b = 0;
@@ -33,6 +64,8 @@ for(var i=1; i<= a; i++){
 }
 console.log("Total sum of even numbers " +b);
 
+// Total sum of odd numbers
+
 var a = 50;
 var b = 0;
 for(var i=1; i<= a; i++){
@@ -43,6 +76,7 @@ for(var i=1; i<= a; i++){
 }
 console.log("Total sum of odd numbers " +b);
 
+// Sum of Array elements
 var array = [10, 2, 40, 23, 40];
 var a = 0;
 for (var i = 0; i <array.length; i++){
@@ -55,3 +89,14 @@ for (var i = 0; i <array.length; i++){
 var arr = [1,2,3,4,5]
 var rev = arr.reverse();
 console.log(rev)
+
+var num = [10,14,3,45,74,1,9]
+
+function sorting(a, b){
+  return (a-b);
+  
+}
+console.log(num)
+
+
+
